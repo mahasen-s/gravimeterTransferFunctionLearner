5780f5c013830c331881b13033eda124c5639efc
Mahasen Sooriyabandara <mahasen.sooriyabandara@gmail.com>
2018-08-20 18:24:26 +1000

    Added code to infer and plot transfer function

--- get_acc_subsequences.m
+++ get_acc_subsequences.m
@@ -10,7 +10,7 @@ acc_col             = 2;
 
 first_half_length   = max(cat(1,inds.inds_u-inds.inds_u_ini,inds.inds_d-inds.inds_d_ini));
 second_half_length  = max(cat(1,inds.inds_u_end-inds.inds_u,inds.inds_d_end-inds.inds_d));
-subseq_length     = first_half_length + second_half_length;
+subseq_length       = first_half_length + second_half_length;
 
 % Reassign indices
 inds.inds_u_init    = inds.inds_u - first_half_length;
---
+++ get_preprocessed_data_4a.m
@@ -0,0 +1,15 @@
+function data = get_preprocessed_data_4a(data)
+% We need to throwaway all points before a certain time since the data is
+% corrupted by temperature changes.
+t_cutoff = 3.633399834260200e+09;
+t_delay  = 100e-3; % This is the same as in get_acc_inds
+
+t_cutoff_ai     = t_cutoff+t_delay+data.T+2*data.tau;
+
+cut_fun  = @(x,t) x(x>t,:);
+
+data.acc        = cut_fun(data.acc,t_cutoff);
+data.ai_u       = cut_fun(data.ai_u,t_cutoff_ai);
+data.ai_d       = cut_fun(data.ai_d,t_cutoff_ai);
+
+end
